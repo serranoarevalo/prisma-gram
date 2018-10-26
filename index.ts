5845e3aedf74017fd01d64d6d49e48f41f7717b7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-26 22:43:55 +0900

    Basic Queries and Mutations

--- index.ts
+++ index.ts
@@ -4,7 +4,13 @@ import { prisma } from "./generated/prisma-client";
 const resolvers = {
   Query: {
     getAllUsers: () => prisma.users(),
-    getAllPhotos: () => prisma.photos()
+    getAllPhotos: () => prisma.photos(),
+    photosByUser: (_, { id }) => prisma.user({ id }).photos()
+  },
+  Mutation: {
+    createUser: (_, { name }) => prisma.createUser({ name }),
+    createPhoto: (_, { caption, userID }) =>
+      prisma.createPhoto({ caption, author: { connect: { id: userID } } })
   }
 };
 
